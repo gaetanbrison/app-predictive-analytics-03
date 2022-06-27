21d9a58a3c08e5c1c11683942e86172134ef94b1
Gaetan Brison <gaetan.brison@gmail.com>
2022-06-27 16:03:42 +0200

    update

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ from datetime import date, datetime
 from pandas import read_csv
 from pandas import to_datetime
 from pandas import DataFrame
-from fbprophet import Prophet
+from prophet import Prophet
 from matplotlib import pyplot
 from sklearn.metrics import mean_absolute_error
 
